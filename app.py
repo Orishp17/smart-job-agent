c939ec79e17fb34a305aefd5dc61d4415d1746c2
Orishp17 <orishp17@gmail.com>
2026-04-20 15:48:26 +0300

    Implement Telegram message sending functionality
    Update app.py to send Telegram message

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-print("Hello Ori - your job alert project has started!")
+import os
+import requests
+
+bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
+chat_id = os.getenv("TELEGRAM_CHAT_ID")
+
+message = "שלום אורי - app.py רץ מתוך GitHub ושולח הודעה אמיתית דרך Python."
+
+url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+
+payload = {
+    "chat_id": chat_id,
+    "text": message
+}
+
+response = requests.post(url, data=payload)
+
+print("Status code:", response.status_code)
+print("Response:", response.text)
